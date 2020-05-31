a4ce29723b8bee79d0ef5948fc883c14da35a85c
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-05-31 20:07:05 +0300

    Refactor the tests and add better frameworks

--- Fraction/src/main/java/FractionMV/Fraction.java
+++ Fraction/src/main/java/FractionMV/Fraction.java
@@ -1,15 +1,17 @@
 package FractionMV;
 
-/**
- * Hello world!
- *
- */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Fraction {
     private int numerator, denominator;
-    public Fraction(int num, int den) {
-        numerator = num;
-        denominator = den;
-    }
+
     public void Simplify() {
         long gcd = GCD(denominator, numerator);
         denominator /= gcd;
@@ -19,22 +21,8 @@ public class Fraction {
             denominator = -denominator;
             numerator = -numerator;
         }
-
-
     }
     private static int GCD(int a, int b) {
         return b == 0 ? a : GCD(b, a % b);
-    }
-    public int getDenominator() {
-        return denominator;
-    }
-    public int getNumerator() {
-        return numerator;
-    }
-    public void setDenominator(int i) {
-        denominator = i;
-    }
-    public void setNumerator(int i) {
-        numerator = i;
     }
 }
--- Fraction/src/test/java/FractionMV/FractionTest.java
+++ Fraction/src/test/java/FractionMV/FractionTest.java
@@ -1,86 +1,58 @@
 package FractionMV;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import org.junit.Test;
-import org.junit.Before;
+import java.util.stream.Stream;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-/**
- * Unit test for simple App.
- */
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FractionTest {
 
-    private Fraction fc1, fc2, fc3;
+    private Fraction fraction;
 
-    @Test
-    public void testSimplify() {
-        try {
-            String[] arrayNumbers = getNumbersFromFile("src/test/java/FractionMV/GivenNumbers1.txt");
-            fc1 = new Fraction(Integer.getInteger(arrayNumbers[0]).intValue(), Integer.getInteger(arrayNumbers[1]).intValue());
-            fc1.Simplify();
-            assertEquals(2, fc1.getNumerator());
-            assertEquals(5, fc1.getDenominator());
-
-            String[] arrayNumbers2 = getNumbersFromFile("src/test/java/FractionMV/GivenNumbers2.txt");
-            fc2 = new Fraction(Integer.getInteger(arrayNumbers2[0]).intValue(), Integer.getInteger(arrayNumbers2[1]).intValue());
-            fc2.Simplify();
-            assertEquals(-25, fc2.getNumerator());
-            assertEquals(7, fc2.getDenominator());
-        }catch(IOException e){
-            System.out.println("IOException ... ");
-        }
+    @BeforeEach
+    void setUpFraction() {
+        this.fraction = new Fraction(12, 13);
     }
 
     @Test
-    public void test_Denominator() {
-        try {
-            String[] arrayNumbers = getNumbersFromFile("src/test/java/FractionMV/GivenNumbers1.txt");
-            fc1 = new Fraction(Integer.getInteger(arrayNumbers[0]).intValue(), Integer.getInteger(arrayNumbers[1]).intValue());
-            int result = fc1.getDenominator();
-            assertTrue("getDenominator() returned " + result + " instead of 30.", result == 30);
-
-            String[] arrayNumbers2 = getNumbersFromFile("src/test/java/FractionMV/GivenNumbers2.txt");
-            fc2 = new Fraction(Integer.getInteger(arrayNumbers2[0]).intValue(), Integer.getInteger(arrayNumbers2[1]).intValue());
-            result = fc2.getNumerator();
-            assertEquals(7, result);
-
-            fc1.setDenominator(1);
-            int result2 = fc1.getDenominator();
-            assertTrue("getDenominator() returned " + result2 + " instead of 1.", result == 1);
-            fc2.setNumerator(6);
-            result2 = fc2.getNumerator();
-            assertEquals(6, result2);
-        }catch(IOException e){
-            System.out.println("IOException ... ");
-        }
+    void getNumerator() {
+        assertEquals(this.fraction.getNumerator(), 12);
     }
 
-    public String[] getNumbersFromFile(String numbersFile) throws IOException
-    {
-        int n = 0;
-        BufferedReader in = new BufferedReader(new FileReader(numbersFile));
-        while ((in.readLine()) != null) {
-            n++;
-        }
-        in.close();
-
-        String[] la=new String[n];
-        String s = new String();
-        int i = 0;
-        in = new BufferedReader(new FileReader(numbersFile));
-        while ((s=in.readLine()) != null) {
-            la[i] = s;
-            i++;
-        }
-        in.close();
-        return la;
+    @Test
+    void getDenominator() {
+        assertEquals(this.fraction.getDenominator(), 13);
     }
 
+    @Test
+    void setNumerator() {
+        this.fraction.setNumerator(11);
+        assertEquals(this.fraction.getNumerator(), 11);
+    }
 
+    @Test
+    void setDenominator() {
+        this.fraction.setNumerator(15);
+        assertEquals(this.fraction.getNumerator(), 15);
+    }
+
+    @ParameterizedTest
+    @MethodSource("simplifyFractionsProvider")
+    public void testSimplify(Fraction first, Fraction second) {
+        first.Simplify();
+        assertEquals(first.getNumerator(), second.getNumerator());
+        assertEquals(first.getDenominator(), second.getDenominator());
+    }
+
+    static Stream<Arguments> simplifyFractionsProvider() {
+        return Stream.of(
+                Arguments.of(new Fraction(12, 30), new Fraction(2, 5)),
+                Arguments.of(new Fraction(-25, 7), new Fraction(-25, 7))
+        );
+    }
 }
